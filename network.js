8e00e46ffd655b4cd3a99549b9e8e295ab88ba8e
NoHaxJustPi <qkilcullen@gmail.com>
2024-02-03 17:31:27 -0600

    URL Parameters, no arrows
    URLParams support (teivel path removal, physics options)
    bidirectional paths now render without arrows

--- network.js
+++ network.js
@@ -1,3 +1,6 @@
+const settings = new URLSearchParams(window.location.search)
+
+
 function resize() {
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
@@ -49,90 +52,110 @@ var nodes = new vis.DataSet([
     { id: 42, label: "Woods II" },
 ]);
 
+dual = false
+
 // create an array with edges
-var edges = new vis.DataSet([
+var raw_edges = [
     // Beach House
-    {from: 1, to: 2, arrows:{to:{enabled: true}, from:{enabled: true}}}, // Beach House <-> Catacombs
-    {from: 1, to: 32, arrows:{to:{enabled: true}, from:{enabled: true}}}, // Beach House <-> Catacombs
+    {from: 1, to: 2, arrows:{to:{enabled: dual}, from:{enabled: dual}}}, // Beach House <-> Catacombs
+    {from: 1, to: 32, arrows:{to:{enabled: dual}, from:{enabled: dual}}}, // Beach House <-> Catacombs
     // Cavern (Catacombs omitted - all connections exhausted) 
-    {from: 3, to: 7, arrows:{to:{enabled: true}, from:{enabled: true}}}, // Cavern <-> Drainage System
+    {from: 3, to: 7, arrows:{to:{enabled: dual}, from:{enabled: dual}}}, // Cavern <-> Drainage System
     {from: 3, to: 11, arrows:{to:{enabled: true}}}, // Cavern -> Far Below
-    {from: 3, to: 18, arrows:{to:{enabled: true}, from:{enabled: true}}}, // Cavern <-> Interior
-    {from: 3, to: 19, arrows:{to:{enabled: true}, from:{enabled: true}}}, // Cavern <-> Laboratory
-    {from: 3, to: 33, arrows:{to:{enabled: true}, from:{enabled: true}}}, // Cavern <-> Shroomcave
+    {from: 3, to: 18, arrows:{to:{enabled: dual}, from:{enabled: dual}}}, // Cavern <-> Interior
+    {from: 3, to: 19, arrows:{to:{enabled: dual}, from:{enabled: dual}}}, // Cavern <-> Laboratory
+    {from: 3, to: 33, arrows:{to:{enabled: dual}, from:{enabled: dual}}}, // Cavern <-> Shroomcave
     // Chess
-    {from: 4, to: 14, arrows:{to:{enabled: true}, from:{enabled: true}}}, // Chess <-> Garden
+    {from: 4, to: 14, arrows:{to:{enabled: dual}, from:{enabled: dual}}}, // Chess <-> Garden
     // Classroom 1917
-    {from: 5, to: 10, arrows:{to:{enabled: true}, from:{enabled: true}}}, // Classroom 1917 <-> Failure
-    {from: 5, to: 29, arrows:{to:{enabled: true}, from:{enabled: true}}}, // Classroom 1917 <-> Public Urinal
+    {from: 5, to: 10, arrows:{to:{enabled: dual}, from:{enabled: dual}}}, // Classroom 1917 <-> Failure
+    {from: 5, to: 29, arrows:{to:{enabled: dual}, from:{enabled: dual}}}, // Classroom 1917 <-> Public Urinal
     // Consumerism
-    {from: 6, to: 23, arrows:{to:{enabled: true}, from:{enabled: true}}}, // Consumerism <-> Office
+    {from: 6, to: 23, arrows:{to:{enabled: dual}, from:{enabled: dual}}}, // Consumerism <-> Office
     // Drainage System
-    {from: 7, to: 8, arrows:{to:{enabled: true}, from:{enabled: true}}}, // Drainage System <-> Drowned
-    {from: 7, to: 29, arrows:{to:{enabled: true}, from:{enabled: true}}}, // Drainage System <-> Public Urinal
+    {from: 7, to: 8, arrows:{to:{enabled: dual}, from:{enabled: dual}}}, // Drainage System <-> Drowned
+    {from: 7, to: 29, arrows:{to:{enabled: dual}, from:{enabled: dual}}}, // Drainage System <-> Public Urinal
     // Drowned
-    {from: 8, to: 14, arrows:{to:{enabled: true}, from:{enabled: true}}}, // Drowned <-> Garden
-    {from: 8, to: 32, arrows:{to:{enabled: true}, from:{enabled: true}}}, // Drowned <-> Shoreline
-    {from: 8, to: 35, arrows:{to:{enabled: true}, from:{enabled: true}}}, // Drowned <-> The Mist
-    {from: 8, to: 42, arrows:{to:{enabled: true}, from:{enabled: true}}}, // Drowned <-> Woods II
+    {from: 8, to: 14, arrows:{to:{enabled: dual}, from:{enabled: dual}}}, // Drowned <-> Garden
+    {from: 8, to: 32, arrows:{to:{enabled: dual}, from:{enabled: dual}}}, // Drowned <-> Shoreline
+    {from: 8, to: 35, arrows:{to:{enabled: dual}, from:{enabled: dual}}}, // Drowned <-> The Mist
+    {from: 8, to: 42, arrows:{to:{enabled: dual}, from:{enabled: dual}}}, // Drowned <-> Woods II
     // Dune
-    {from: 9, to: 31, arrows:{to:{enabled: true}, from:{enabled: true}}}, // Dune <-> Sandland
+    {from: 9, to: 31, arrows:{to:{enabled: dual}, from:{enabled: dual}}}, // Dune <-> Sandland
     // Failure
-    {from: 10, to: 19, arrows:{to:{enabled: true}, from:{enabled: true}}}, // Failure <-> Laboratory
+    {from: 10, to: 19, arrows:{to:{enabled: dual}, from:{enabled: dual}}}, // Failure <-> Laboratory
     // Far Below
     {from: 11, to: 34, arrows:{to:{enabled: true}}}, // Far Below -> Tievel
     // Fidelity
     {from: 12, to: 14, arrows:{to:{enabled: true}}}, // Fidelity -> Garden
     // Frigid
-    {from: 13, to: 21, arrows:{to:{enabled: true}, from:{enabled: true}}}, // Frigid <-> Northtorn
-    {from: 13, to: 24, arrows:{to:{enabled: true}, from:{enabled: true}}}, // Frigid <-> Outskirts
+    {from: 13, to: 21, arrows:{to:{enabled: dual}, from:{enabled: dual}}}, // Frigid <-> Northtorn
+    {from: 13, to: 24, arrows:{to:{enabled: dual}, from:{enabled: dual}}}, // Frigid <-> Outskirts
     {from: 13, to: 35, arrows:{to:{enabled: true}}}, // Frigid -> The Mist
     // Garden
-    {from: 14, to: 38, arrows:{to:{enabled: true}, from:{enabled: true}}}, // Garden <-> The Woods
+    {from: 14, to: 38, arrows:{to:{enabled: dual}, from:{enabled: dual}}}, // Garden <-> The Woods
     // Great Hall
     {from: 15, to: 12, arrows:{to:{enabled: true}}}, // Great Hall -> Fidelity
-    {from: 15, to: 16, arrows:{to:{enabled: true}, from:{enabled: true}}}, // Great Hall <-> Home 3
-    {from: 15, to: 26, arrows:{to:{enabled: true}, from:{enabled: true}}}, // Great Hall <-> Patience
-    {from: 15, to: 41, arrows:{to:{enabled: true}, from:{enabled: true}}}, // Great Hall <-> Void
+    {from: 15, to: 16, arrows:{to:{enabled: dual}, from:{enabled: dual}}}, // Great Hall <-> Home 3
+    {from: 15, to: 26, arrows:{to:{enabled: dual}, from:{enabled: dual}}}, // Great Hall <-> Patience
+    {from: 15, to: 41, arrows:{to:{enabled: dual}, from:{enabled: dual}}}, // Great Hall <-> Void
     // Home 3
-    {from: 16, to: 18, arrows:{to:{enabled: true}, from:{enabled: true}}}, // Home 3 <-> Interior
-    {from: 16, to: 20, arrows:{to:{enabled: true}, from:{enabled: true}}}, // Home 3 <-> Marble Gallery
-    {from: 16, to: 38, arrows:{to:{enabled: true}, from:{enabled: true}}}, // Home 3 <-> The Woods
+    {from: 16, to: 18, arrows:{to:{enabled: dual}, from:{enabled: dual}}}, // Home 3 <-> Interior
+    {from: 16, to: 20, arrows:{to:{enabled: dual}, from:{enabled: dual}}}, // Home 3 <-> Marble Gallery
+    {from: 16, to: 38, arrows:{to:{enabled: dual}, from:{enabled: dual}}}, // Home 3 <-> The Woods
     // Hospital
-    {from: 17, to: 23, arrows:{to:{enabled: true}, from:{enabled: true}}}, // Hospital <-> Office
+    {from: 17, to: 23, arrows:{to:{enabled: dual}, from:{enabled: dual}}}, // Hospital <-> Office
     // Interior
-    {from: 18, to: 36, arrows:{to:{enabled: true}, from:{enabled: true}}}, // Interior <-> The Room
-    {from: 18, to: 30, arrows:{to:{enabled: true}, from:{enabled: true}}}, // Interior <-> Punishment Corner
+    {from: 18, to: 36, arrows:{to:{enabled: dual}, from:{enabled: dual}}}, // Interior <-> The Room
+    {from: 18, to: 30, arrows:{to:{enabled: dual}, from:{enabled: dual}}}, // Interior <-> Punishment Corner
     // Laboratory
-    {from: 19, to: 23, arrows:{to:{enabled: true}, from:{enabled: true}}}, // Laboratory <-> Office
-    {from: 19, to: 24, arrows:{to:{enabled: true}, from:{enabled: true}}}, // Laboratory <-> Outskirts
-    {from: 19, to: 27, arrows:{to:{enabled: true}, from:{enabled: true}}}, // Laboratory <-> Patients
-    {from: 19, to: 28, arrows:{to:{enabled: true}, from:{enabled: true}}}, // Laboratory <-> Preserve
+    {from: 19, to: 23, arrows:{to:{enabled: dual}, from:{enabled: dual}}}, // Laboratory <-> Office
+    {from: 19, to: 24, arrows:{to:{enabled: dual}, from:{enabled: dual}}}, // Laboratory <-> Outskirts
+    {from: 19, to: 27, arrows:{to:{enabled: dual}, from:{enabled: dual}}}, // Laboratory <-> Patients
+    {from: 19, to: 28, arrows:{to:{enabled: dual}, from:{enabled: dual}}}, // Laboratory <-> Preserve
     // Marble Gallery
-    {from: 20, to: 25, arrows:{to:{enabled: true}, from:{enabled: true}}}, // Marble Gallery <-> Paradoxical
+    {from: 20, to: 25, arrows:{to:{enabled: dual}, from:{enabled: dual}}}, // Marble Gallery <-> Paradoxical
     // Obsidian Gallery (Northtorn omitted - all connections exhausted)
-    {from: 22, to: 25, arrows:{to:{enabled: true}, from:{enabled: true}}}, // Obsidian Gallery <-> Paradoxical
+    {from: 22, to: 25, arrows:{to:{enabled: dual}, from:{enabled: dual}}}, // Obsidian Gallery <-> Paradoxical
     // Outskirts (Office omitted - yeah)
     {from: 24, to: 40, arrows:{to:{enabled: true}}}, // Outskirts -> Vein
     // Preserve (Paradoxical, Patience, Patients omitted)
     {from: 28, to: 1, arrows:{to:{enabled: true}}}, // Preserve -> Beach House
-    {from: 28, to: 38, arrows:{to:{enabled: true}, from:{enabled: true}}}, // Preserve <-> The Woods
+    {from: 28, to: 38, arrows:{to:{enabled: dual}, from:{enabled: dual}}}, // Preserve <-> The Woods
     // Sandland (Public Urinal, Punishment Corner omitted)
-    {from: 31, to: 32, arrows:{to:{enabled: true}, from:{enabled: true}}}, // Sandland <-> Shoreline
-    {from: 31, to: 39, arrows:{to:{enabled: true}, from:{enabled: true}}}, // Sandland <-> Tomb
+    {from: 31, to: 32, arrows:{to:{enabled: dual}, from:{enabled: dual}}}, // Sandland <-> Shoreline
+    {from: 31, to: 39, arrows:{to:{enabled: dual}, from:{enabled: dual}}}, // Sandland <-> Tomb
     // Shroomcave (Shoreline omitted)
-    {from: 33, to: 42, arrows:{to:{enabled: true}, from:{enabled: true}}}, // Shroomcave <-> Woods II
+    {from: 33, to: 42, arrows:{to:{enabled: dual}, from:{enabled: dual}}}, // Shroomcave <-> Woods II
     // Teivel
     {from: 34, to: 26, arrows:{to:{enabled: true}}}, // Teivel -> Patience
     // The Mist
-    {from: 35, to: 37, arrows:{to:{enabled: true}, from:{enabled: true}}}, // The Mist <-> The Silence 
+    {from: 35, to: 37, arrows:{to:{enabled: dual}, from:{enabled: dual}}}, // The Mist <-> The Silence 
     // The Woods (The Room, The Silence omitted)
     {from: 38, to: 3, arrows:{to:{enabled: true}}}, // The Woods -> Cavern
     // Vein (Tomb omitted)
     {from: 40, to: 17, arrows:{to:{enabled: true}}}, // The Woods -> Cavern
     // Woods II (Void omitted)
     {from: 42, to: 36, arrows:{to:{enabled: true}}}, // Woods II -> The Room
-]);
+]
+
+if (settings.get('teivel') == 'false') {
+    raw_edges.splice(47, 1) // increase the 47 if items are added before Exit Teivel
+}
+
+edges = new vis.DataSet(raw_edges)
+checkNum = function(name) {
+    let val = settings.get(name)
+    if (!isNaN(val) && !isNaN(parseFloat(val))) {
+        return parseFloat(val)
+    }
+}
+
+checkBool = function(name) {
+    val = settings.get(name)
+    if (val == 'true') return true
+    else if (val == 'false') return false
+}
 
 // create a network
 var container = document.getElementById("mynetwork");
@@ -143,7 +166,20 @@ var data = {
 var options = {
     edges: {
         color: 'red',
-        style: 'arrow-center'
+        smooth: checkBool('physics') ?? true
+    },
+    physics: {
+        enabled: checkBool('physics') ?? true,
+        barnesHut: {
+            theta: checkNum('theta') ?? 0.5,
+            gravitationalConstant: checkNum('grav') ?? -2000,
+            centralGravity: checkNum('centGrav') ?? 0.3,
+            springLength: checkNum('springLen') ?? 95,
+            springConstant: checkNum('springConst') ?? 0.04,
+            damping: checkNum('damping') ?? 0.09,
+        },
+        maxVelocity: checkNum('maxVel') ?? 50,
+        minVelocity: checkNum('minVel') ?? 0.1
     }
 };
 var network = new vis.Network(container, data, options);
